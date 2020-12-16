f893fcec13aa29a47f96306493f431ef64a13016
Lilyokalani Hafoka <lilyhafoka.du@gmail.com>
2020-12-15 21:48:18 -0700

    fixed index.js issues

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,17 +1,36 @@
-const noteData = require('../db/db.json')
+// const noteData = require('../db/db.json')
+const fs = require('fs')
+const util = require('util')
 
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 module.exports = (app) => {
-    app.get('/api/notes', (req, res) => res.json(noteData));
+    app.get('/api/notes', (req, res) => readFileAsync('./db/db.json', 'utf8').then((data) => {
+      res.json(data);
+    }));
+
 
     app.post('/api/notes', (req, res) => {
       let newNote = req.body;
 
       console.log(newNote);
+      readFileAsync('./db/db.json', 'utf8').then(data => {
 
-      noteData.push(newNote);
-      res.json(newNote)  
+        let parsedData = JSON.parse(data)
+        let updatedNotes = [...parsedData, newNote];
+
+        return updatedNotes;
+      }).then(parsedNotes => {
+        return writeFileAsync('./db/db.json', JSON.stringify(parsedNotes))
+      .then(() => {
+        res.json(newNote);
+      })
+      })  
+      // noteData.push(newNote);
+      // res.json(newNote)
+      // res.end();
     })
 
-    
-};
+
+  }
--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,17 +1,3 @@
-// const path = require("path");
-
-
-// module.exports = (app) => {
-//   //GET REQUESTS
-//   app.get("/notes", (req, res) => {
-//     res.sendFile(path.join(__dirname, "notes.html"));
-//   });
-//   //DEFAULTS TO HOME PAGE
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "index.html"));
-//   });
-// };
-
 module.exports = (app) => {
 
     app.get('/notes', (req, res) => {
